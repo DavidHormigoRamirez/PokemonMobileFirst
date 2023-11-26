aedf1aa51bcccfa507a058ee376698099faa78a7
David Hormigo Ramírez <dhorram948@g.educaand.es>
2023-11-26 12:20:18 +0100

    Bottom navigation

--- app/src/main/java/com/alanturing/cpifp/pokemonroom/ui/MainActivity.kt
+++ app/src/main/java/com/alanturing/cpifp/pokemonroom/ui/MainActivity.kt
@@ -2,13 +2,38 @@ package com.alanturing.cpifp.pokemonroom.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.alanturing.cpifp.pokemonroom.R
+import com.alanturing.cpifp.pokemonroom.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navController = navHostFragment.navController
+        //val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.pokemonListFragment,
+                R.id.locationListFragment
+            )
+        )
+        navView.setupWithNavController(navController)
+        //setupActionBarWithNavController(navController,appBarConfiguration)
+
     }
 }
